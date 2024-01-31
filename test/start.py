9f81f4567fc6137edb29f2eb1d49ba836944f44e
Dmytro Oliinyk <149588780+dm-ol@users.noreply.github.com>
2024-01-31 13:02:02 +0200

    Create start.py

---
+++ test/start.py
@@ -0,0 +1,40 @@
+# Це перші спроби розібратися з кодом Python
+print("Тут починається код")
+
+# Математика проста та не дуже
+print ("Звичайна математка")
+print("1992+22-444*3=")
+print (1992+22-444*3)
+
+# String data
+print("Тип String (Строка)")
+print ("Текст, пустий список та інше")
+
+# Integer
+print("Тип Integer (Звичайне число)")
+print (1999)
+print (444)
+
+# Float
+print("Тип Float (Дробне число)")
+print (10.2)
+print (-3.1)
+
+# Boolean
+print("Тип Boolean (Порівняння)")
+print ("10>4")
+print (10>4)
+
+# List
+print("Тип List (Список)")
+print ("список1", "список2", "список3")
+
+# Змінні та їх значення
+zero_day = "23_02_2022"
+device_id = "f19zr5m"
+print(zero_day)
+print(device_id)
+
+# Функція type
+data_type = type(device_id)
+print(data_type)
