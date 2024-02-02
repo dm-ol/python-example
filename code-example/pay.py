e3530dcf774cd9d87b0e668c8091251b40edafee
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-02-02 19:08:09 +0200

    Add some examples

---
+++ code-example/pay.py
@@ -0,0 +1,4 @@
+hours = input("Enter Hours:")
+rate = input("Enter Payment rate:")
+pay = float(hours) * float(rate)
+print ("Pay:", pay)
---
+++ code-example/wordcount.py
@@ -0,0 +1,22 @@
+# Отримати ім’я файлу та відкрити його
+name = input('Enter file:') 
+handle = open(name, 'r')
+
+# Рахувати частоту слів
+counts = dict()
+for line in handle: 
+    words = line.split() 
+    for word in words:
+        counts[word] = counts.get(word,0) + 1
+
+# Знайти найуживаніше слово
+bigcount = None 
+bigword = None
+for word,count in counts.items():
+    if bigcount is None or count > bigcount: 
+        bigword = word
+        bigcount = count
+
+# Готово
+print("Найвживаніше слово:", bigword)
+print("Кількість раз:", bigcount)
