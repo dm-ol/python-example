f5f12d8b8a8386701e1f8137242885c8582bae9d
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-02-04 22:23:59 +0200

    Add

---
+++ test/compare.py
@@ -0,0 +1,17 @@
+from functools import total_ordering
+
+@total_ordering
+class Number:
+   def __init__(self, value):
+      self.value = value
+
+   def __lt__(self, other):
+      return self.value < other.value
+
+   def __eq__(self, other):
+      return self.value == other.value
+
+print(Number(20) > Number(3))
+print(Number(1) < Number(5))
+print(Number(15) >= Number(15))
+print(Number(10) < Number(2))
---
+++ test/enum.py
@@ -0,0 +1,17 @@
+from colormap import rgb2hex
+from enum import Enum
+
+class Color(Enum):
+   RED = (255, 0, 0)
+   GREEN = (0, 255, 0)
+   BLUE = (0, 0, 255)
+
+   @property
+   def hex(self):
+      return rgb2hex(*self.value)
+
+Color.RED # <Color. RED: (255, 0, 0)>
+Color.RED.name # 'RED'
+Color.RED.value # (255, 0, 0)
+
+Color.RED.hex # '#FF0000'
