309a85ebdcc3b938f5cb2ba5e04da61a1c64d087
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-02-06 23:50:12 +0200

    add examples

---
+++ code-example/largest-from.py
@@ -0,0 +1,25 @@
+# Зчитайте три числа
+try:
+    number1 = int(input("Введіть перше число: "))
+    number2 = int(input("Введіть друге число: "))
+    number3 = int(input("Введіть третє число: "))
+except ValueError:
+    print("Помилка: Будь ласка введіть числове значення.")
+    quit()
+# Тимчасово припустимо, що перше число
+# є найбільшим.
+# Ми незабаром це перевіримо.
+largest_number = number1
+ 
+# Перевіряємо чи друге число більше поточного largest_number
+# і оновлюємо largest_number, якщо потрібно.
+if number2 > largest_number:
+    largest_number = number2
+ 
+# Перевіряємо, чи третє число більше поточного largest_number
+# і оновлюємо largest_number, якщо потрібно.
+if number3 > largest_number:
+    largest_number = number3
+ 
+# Виведіть результат
+print("Найбільше число це:", largest_number)
---
+++ test/min-max.py
@@ -0,0 +1,12 @@
+# Простий приклад роботи min та max
+# Задаємо список чисел
+list_1 = [2, 42, 13, 88, 36, 49, 114, 99, 41, 9, 63, 200]
+# Перевір, яке з чисел найбільше
+# і передайте його largest_number variable.
+ 
+largest_number = max(list_1)
+smallest_number = min(list_1)
+ 
+# Виведіть результат
+print("Найбільше число це:", largest_number)
+print("Найменше число це:", smallest_number)
