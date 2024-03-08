639edb4e83d4579817dab475254f60804f71ad2f
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-03-08 12:22:52 +0200

    some edit

---
+++ python/modules/pytesseract.py
@@ -0,0 +1,21 @@
+# Пакет pytesseract, який дозволяє парсити текст з картинок
+# за допомогою оптичного розпізнавання символів. Щоб отримати
+# текст з картинки, викликаємо метод image_to_string.
+# Якщо текст англійською, вкажіть аргумент lang як 'eng'.
+# Для відкриття картинок можна використовувати pillow,
+# або просто вказати шлях до файлу у вигляді рядка.
+
+from PIL import Image
+import pytesseract
+
+# Конвертуємо картинку в текст та виводимо результат
+print(pytesseract.image_to_string(Image.open('test.png'))
+
+# Вказуємо англійську мову в аргументах
+print(pytesseract.image_to_string('test-english.jpg', lang='eng'))
+
+try:
+   # Задаємо максимальний час очікування
+   print(pytesseract.image_to_string('test.jpg', timeout=2))
+except RuntimeError as timeout_error:
+   pass
