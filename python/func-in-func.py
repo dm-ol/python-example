48a7594fbc8b906e2710e49cc5bb5806791335d5
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-04-14 17:09:28 +0300

    edit func

---
+++ python/func-in-func.py
@@ -0,0 +1,29 @@
+# Функція може використовувати іншу функцію, як параметр. Приклад:
+
+from datetime import date
+
+
+def get_weekday():
+    return date.today().strftime('%A')
+
+# Функція використовує попередню функцію як параметр за замовчуванням
+
+
+def create_new_post(post, weekday=get_weekday()):
+    post_copy = post.copy()
+    post_copy['created_on_weekday'] = weekday
+    return post_copy
+
+# Створюєм аргументи для функції
+
+
+initial_post = {
+    'id': 44,
+    'author': 'DevDM',
+}
+
+# Викликаєм функцію з створеними аргументами
+
+post_with_weekday = create_new_post(initial_post)
+
+print(post_with_weekday)
