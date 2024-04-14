f8b638fab561b677fd74790cf4d2e7abff468b5e
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-04-14 16:04:24 +0300

    edit func

--- python/func-simply.py
+++ python/func-simply.py
@@ -20,3 +20,26 @@ def multi_func(*args):
 # Вказуємо будь-яку кількість аргументів для функції
 multi_sum = multi_func(5, 12, 44, 11, 49)
 print(multi_sum)
+
+# Приклад функції зі строкою
+
+
+def author_info(name, book):
+    info = f"{name} wrote {book} and other books"
+    return info
+
+
+info = author_info("John Do", "Black book")
+print(info)
+
+# Приклад функції з keywords arguments (аргумент з ключовими словами)
+
+
+def author_info(name, book):
+    info = f"{name} wrote {book} and other books"
+    return info
+
+
+# задали аргументи з ключовими словами, порядок не важливий
+info = author_info(book="White book", name="Lindsey Dir")
+print(info)
