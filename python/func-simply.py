bd8a0fa19589048abf63eae13a0a99adcc1b873e
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-04-14 15:03:49 +0300

    edit func

--- python/func-simply.py
+++ python/func-simply.py
@@ -1,5 +1,22 @@
+# Приклад простої функції на 2 параметри
+
 def addtwo(a, b):
     added = a + b
     return added
-x = addtwo(11, 45435)
+
+
+x = addtwo(11, 45435)  # Вказуємо аргументи для функції
 print(x)
+
+# Приклад функції на будь-яку кількість параметрів
+
+
+def multi_func(*args):
+    print(args)
+    print(type(args))
+    return sum(args)
+
+
+# Вказуємо будь-яку кількість аргументів для функції
+multi_sum = multi_func(5, 12, 44, 11, 49)
+print(multi_sum)
