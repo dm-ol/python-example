90443d4ad021656880dfac95033af99c3be19716
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-04-27 14:33:34 +0300

    some edit

--- python/false-value.py
+++ python/false-value.py
@@ -3,3 +3,20 @@
 print(bool(0))  # int value False
 print(bool(0.0))  # float value False
 print(bool(0j))  # complex value False
+
+# False is False
+
+print(bool(False))
+
+# None is False
+
+print(bool(None))
+
+# Empty types is False
+
+print(bool({}))  # Empty dict
+print(bool(()))  # Empty tuple
+print(bool([]))  # Empty list
+print(bool(""))  # Empty str
+print(bool(set()))  # Empty set
+print(bool(range(0)))  # Empty range
