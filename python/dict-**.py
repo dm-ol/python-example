e0c77196d39d60084801cb0ed08430f2e9cf2ed1
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-04-27 22:50:00 +0300

    add **

--- python/dict-**.py
+++ python/dict-**.py
@@ -11,6 +11,24 @@ car_price = {
     "price": 120000
 }
 
-print(car_price)  # Новий словник з ціною
+print(car_price)  # Новий словник з новим ключем Price
 
 print(car)  # Перший словник без змін
+
+
+# Варіант перезапису ключа в словнику
+
+car_2 = {
+    "brand": "Ferrari",
+    "model": "F50",
+    "year": 1995,
+    "price": 500000
+}
+
+car_2_price = {
+    **car_2,  # Беремо словник car та змінюємо значення ключа price
+    "price": 800000
+}
+
+print(car_2_price)
+print(car_2)
