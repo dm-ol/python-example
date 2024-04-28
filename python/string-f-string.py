210508b9e87ae7c423eb4e6a47e4844f7d325ede
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-04-28 14:41:08 +0300

    edit f-strings

--- python/string-f-string.py
+++ python/string-f-string.py
@@ -14,3 +14,12 @@ print(str_3)  # Very good? Yes! Very good!
 
 today = datetime.datetime.today()
 print(f"{today:%B %d, %Y}")
+
+
+# Example
+
+english = 78
+maths = 56
+biology = 85
+
+print(f"Ram got total marks {english + maths + biology} out of 300")
