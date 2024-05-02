5b7fa376f8d98774c2234712754ed9722e3ea6e9
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-02 22:46:56 +0300

    edit errors

--- python/errors.py
+++ python/errors.py
@@ -1,8 +1,20 @@
-# ZeroDivisionError
+# Вивід помилки в коді
 
 try:
-    print(10 / 0)
+    print(a10 / 0)
 except ZeroDivisionError as err:
+    print(type(err))
+    print("Error:", err)
+except TypeError as err:
+    print(type(err))
+    print("Error:", err)
+except SyntaxError as err:
+    print(type(err))
+    print("Error:", err)
+except NameError as err:
+    print(type(err))
+    print("Error:", err)
+except ValueError as err:
     print(type(err))
     print("Error:", err)
 print("Continue...")
