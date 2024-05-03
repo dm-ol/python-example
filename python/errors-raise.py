79c6262b1de9060b53202357a88b54ff1c9141d5
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-03 22:42:39 +0300

    edit raise

--- python/errors-raise.py
+++ python/errors-raise.py
@@ -2,15 +2,23 @@
 # який тип помилки викликати, і текст, який потрібно надрукувати користувачеві.
 
 # Example 1
-x = -1
-
-if x < 0:
-    raise Exception("Sorry, no numbers below zero")
-
-# Example 2
 
 
 def nums_div(a, b):
     if b == 0:
         raise ValueError("Second argument can't be 0")
     return a / b
+
+
+try:
+    nums_div(12, 0)
+except ZeroDivisionError as err:
+    print(err)
+except ValueError as err:
+    print(err)
+
+# Example 2
+x = -1
+
+if x < 0:
+    raise Exception("Sorry, no numbers below zero")
