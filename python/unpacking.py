ea95c0ae32a7ed5172f2e5f5c5eb41674273c59a
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-07 23:16:38 +0300

    edit unpacking

--- python/unpacking.py
+++ python/unpacking.py
@@ -8,3 +8,32 @@ print(car_1)
 print(car_2)
 print(car_3)
 print(car_4)
+
+
+# Розпаковка кортежа (tuple)
+
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+#
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+#
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
