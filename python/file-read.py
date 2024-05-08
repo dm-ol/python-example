8912ac524702fae48521a0d78a413065ecf088c2
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-08 22:29:50 +0300

    edit file

---
+++ python/file-read.py
@@ -0,0 +1,8 @@
+# В Python для читання файлів використовується метод read().
+
+# Відкриваємо файл
+file1 = open("sample.txt", "r")
+
+# Читаємо файл
+read_content = file1.read()
+print(read_content)
