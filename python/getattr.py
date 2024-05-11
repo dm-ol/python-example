898dadcc9ab379dac8b5a3ec3d237e641144f27a
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-11 22:59:12 +0300

    edit getattr

--- python/getattr.py
+++ python/getattr.py
@@ -19,3 +19,15 @@ print(name)
 
 marks = getattr(person, 'marks')
 print(marks)
+
+
+# Example 2
+
+class Person:
+    age = 23
+    name = "Adam"
+
+
+person = Person()
+print('The age is:', getattr(person, "age"))
+print('The age is:', person.age)
