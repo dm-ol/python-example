65a87f28121efe808a308fc44dd2b23a9de573bc
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-12 13:54:33 +0300

    edit filter

--- code-example/teest-filter.py
+++ code-example/teest-filter.py
@@ -2,7 +2,7 @@
 
 def filter_list(list_to_filter, value_type):
     def check_element_type(elem):
-        return isinstance(elem, value_type)
+        return type(elem) is value_type
 
     return list(filter(check_element_type, list_to_filter))
 
