58409c8a52cced845072ad336669ac58b0c696bc
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-17 14:00:24 +0300

    edit iter

--- python/iter.py
+++ python/iter.py
@@ -50,5 +50,25 @@ print(next(print_num_iter))
 print(next(print_num_iter))
 print(next(print_num_iter))
 
-# Викликаємо StopIteration
-print(next(print_num_iter))
+
+# Приклад №3: Функція iter() із параметром sentinel
+
+class DoubleIt:
+
+    def __init__(self):
+        self.start = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.start *= 2
+        return self.start
+
+    __call__ = __next__
+
+
+my_iter = iter(DoubleIt(), 16)
+
+for x in my_iter:
+    print(x)
