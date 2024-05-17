c30f9f1c2ff7384c6431567203fc0a4c58a626dc
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-17 11:39:36 +0300

    edit continue

--- python/continue.py
+++ python/continue.py
@@ -1,4 +1,5 @@
 # continue ‒ поводиться так, ніби програма раптово дійшла до кінця тіла; починається виконання наступного оберту і негайно перевіряється умова.
+import random
 print("\Інструкція continue:")
 for i in range(1, 6):
     if i == 3:
@@ -34,3 +35,16 @@ if counter:
     print("Найбільше число дорівнює", largest_number)
 else:
     print("Ви не ввели жодного числа.")
+
+
+# Ще один приклад роботи continue
+
+
+random_num = random.randint(1, 11)
+while True:
+    num = int(input("Enter the number from 1 to 10: "))
+    if num != random_num:
+        print("Try again!")
+        continue
+    print("Yes, this is ", random_num)
+    break
