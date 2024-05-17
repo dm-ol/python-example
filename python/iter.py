fc8fd1ee089640d6cdb3213c399f36b90399624e
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-17 13:57:26 +0300

    увше iter

--- python/iter.py
+++ python/iter.py
@@ -1,10 +1,24 @@
 # Функція iter() повертає ітератор для вказаного аргументу.
 # Синтаксис функції iter(): iter(object, sentinel)
 
-# Список голосних
+# Список телефонів
 phones = ['apple', 'samsung', 'oneplus']
 phones_iter = iter(phones)
 
 print(next(phones_iter))
 print(next(phones_iter))
 print(next(phones_iter))
+
+# Приклад №1: Функція iter() в Python
+
+# Список голосних
+vowels = ["a", "e", "i", "o", "u"]
+
+# Функція iter() зі списком голосних
+vowels_iter = iter(vowels)
+
+print(next(vowels_iter))
+print(next(vowels_iter))
+print(next(vowels_iter))
+print(next(vowels_iter))
+print(next(vowels_iter))
