a93ed18a08086b88a1d632e6157cef7bb48813ef
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-17 13:07:44 +0300

    update for

--- python/cycle-for-short.py
+++ python/cycle-for-short.py
@@ -2,7 +2,7 @@
 
 numbers = [12, 34, 1, 4, 4, 67, 37, 9, 0, 81]
 
-# Original
+# Original (list)
 
 result = []
 for number in numbers:
@@ -10,13 +10,13 @@ for number in numbers:
         result.append(number)
 print(result)  # Prints [12, 34, 67, 37, 9, 81]
 
-# Shorter
+# Shorter (list)
 
 result = [number for number in numbers if number > 5]
 print(result)  # Prints [12, 34, 67, 37, 9, 81]
 
 
-# Original_2
+# Original_2 (list)
 
 all_nums = [-5, 2, 0, 22, -12, 26, 92, -111]
 
@@ -27,13 +27,13 @@ for num in all_nums:
 print(absolute_nums)
 
 
-# shorter_2
+# shorter_2 (list)
 
 absolute_nums = [abs(num) for num in all_nums]
 print(absolute_nums)
 
 
-# Original_3
+# Original_3 (set)
 
 my_set = {1, 10, 15, 20}
 
@@ -44,6 +44,6 @@ for val in my_set:
 
 print(new_set)
 
-# Shorter_3
+# Shorter_3 (set)
 new_set = {val * val for val in my_set}
 print(new_set)
