70df1c3c871147a5c4abbcdb66d17e31b4cc5277
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-18 15:00:38 +0300

    edit modules

--- python/modules/module-import.py
+++ python/modules/module-import.py
@@ -2,6 +2,12 @@
 
 import math
 
-# Second variant (func from module)
+# Second variant (func from module) (вибірково)
 
-from math import pi
+from math import sin, pi
+
+print(sin(pi/2))
+
+# Third variant (all func from module): from module import *
+
+# from math import *
