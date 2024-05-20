d001fd00903755f045ceb1db032db35941c889e8
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-20 22:40:24 +0300

    add ord

---
+++ python/ord.py
@@ -0,0 +1,11 @@
+# Функція ord() повертає цілочисельне представлення Unicode-символу.
+
+character = 'P'
+
+# Визначаємо цілочисельне представлення символу P
+unicode_char = ord(character)
+print(unicode_char)
+
+print(ord('5'))
+print(ord('A'))
+print(ord('$'))
