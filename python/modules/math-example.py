5e53590bb54c2cb83d823c485431dc2c54f57730
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-25 15:24:18 +0300

    edit math

--- python/modules/math-example.py
+++ python/modules/math-example.py
@@ -1,5 +1,6 @@
-#
+# Вимірювання кутів
 
+from math import e, exp, log
 from math import pi, radians, degrees, sin, cos, tan, asin
 
 ad = 90
@@ -10,3 +11,11 @@ print(ad == 90.)
 print(ar == pi / 2.)
 print(sin(ar) / cos(ar) == tan(ar))
 print(asin(sin(ar)) == ar)
+
+
+# Інша група з math утворюють функції, які пов'язані з піднесенням до степеня :
+
+
+print(pow(e, 1) == exp(log(e)))
+print(pow(2, 2) == exp(2 * log(2)))
+print(log(e, e) == exp(0))
