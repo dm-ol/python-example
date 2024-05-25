23666274a13bfd441842b7fbe69160bf9eccd7ef
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-05-25 15:26:35 +0300

    edit math

--- python/modules/math-example.py
+++ python/modules/math-example.py
@@ -1,5 +1,6 @@
 # Вимірювання кутів
 
+from math import ceil, floor, trunc
 from math import e, exp, log
 from math import pi, radians, degrees, sin, cos, tan, asin
 
@@ -19,3 +20,17 @@ print(asin(sin(ar)) == ar)
 print(pow(e, 1) == exp(log(e)))
 print(pow(2, 2) == exp(2 * log(2)))
 print(log(e, e) == exp(0))
+
+
+# Порівняння чисел
+
+
+x = 1.4
+y = 2.6
+
+print(floor(x), floor(y))
+print(floor(-x), floor(-y))
+print(ceil(x), ceil(y))
+print(ceil(-x), ceil(-y))
+print(trunc(x), trunc(y))
+print(trunc(-x), trunc(-y))
