2813f8022fa2879915759bd945f1feb4e4d9928b
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-04 12:02:10 +0000

    add platform

---
+++ python/platform.py
@@ -0,0 +1,8 @@
+# Access to underlying platform’s identifying data
+import platform
+
+print('Platform processor:', platform.processor())
+print('Operating system:', platform.system())
+platform.architecture()
+platform.machine()
+platform.python_compiler()
