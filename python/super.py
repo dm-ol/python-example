8ef7a671a0c6804ee5d1e354186db69b693dab21
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-07 14:42:07 +0000

    edit super

--- python/super.py
+++ python/super.py
@@ -14,3 +14,56 @@ class Mammal(Animal):
 
 
 dog = Mammal()
+
+# Приклад №1: Функція super() з одинарним успадкуванням.
+# У разі одинарного успадкування ми використовуємо функцію super() для посилання на базовий клас:
+
+
+class Mammal(object):
+    def __init__(self, mammalName):
+        print(mammalName, 'is a warm-blooded animal.')
+
+
+class Dog(Mammal):
+    def __init__(self):
+        print('Dog has four legs.')
+        super().__init__('Dog')
+
+
+d1 = Dog()
+
+# Приклад №2: Функція super() з множинним успадкуванням.
+
+
+class Animal:
+    def __init__(self, Animal):
+        print(Animal, 'is an animal.')
+
+
+class Mammal(Animal):
+    def __init__(self, mammalName):
+        print(mammalName, 'is a warm-blooded animal.')
+        super().__init__(mammalName)
+
+
+class NonWingedMammal(Mammal):
+    def __init__(self, NonWingedMammal):
+        print(NonWingedMammal, "can't fly.")
+        super().__init__(NonWingedMammal)
+
+
+class NonMarineMammal(Mammal):
+    def __init__(self, NonMarineMammal):
+        print(NonMarineMammal, "can't swim.")
+        super().__init__(NonMarineMammal)
+
+
+class Dog(NonMarineMammal, NonWingedMammal):
+    def __init__(self):
+        print('Dog has 4 legs.')
+        super().__init__('Dog')
+
+
+d = Dog()
+print('')
+bat = NonMarineMammal('Bat')
