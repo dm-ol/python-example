67f69c1c12e0bec0a78b7bbaba87bcf3ab2493d3
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-08 19:39:20 +0000

    edit slice

--- python/slice.py
+++ python/slice.py
@@ -26,3 +26,26 @@ print(py_string[slice_object])
 # Містить індекси 1, 3 та 5
 slice_object = slice(1, 6, 2)
 print(py_string[slice_object])
+
+# Приклад №3: Отримання підрядка за допомогою від’ємного індексу
+
+py_string = 'Python'
+
+# start = -1, stop = -4, step = -1
+# Містить індекси -1, -2 та -3
+slice_object = slice(-1, -4, -1)
+
+print(py_string[slice_object])
+
+# Приклад №4: Отримання підсписку та підкортежу
+
+py_list = ['P', 'y', 't', 'h', 'o', 'n']
+py_tuple = ('P', 'y', 't', 'h', 'o', 'n')
+
+# Містить індекси 0, 1 та 2
+slice_object = slice(3)
+print(py_list[slice_object])
+
+# Містить індекси 1 та 3
+slice_object = slice(1, 5, 2)
+print(py_tuple[slice_object])
