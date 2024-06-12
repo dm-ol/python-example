909a6ba067d50cbf3e706a048523db3d18e07b49
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-12 20:04:28 +0000

    edit module

--- python/test-mod/main.py
+++ python/test-mod/main.py
@@ -1,2 +1,7 @@
 # Коли модуль імпортується, його вміст неявно виконується Python
-import module
+from module import suml, prodl
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(suml(zeroes))
+print(prodl(ones))
