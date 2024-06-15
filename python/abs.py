74c7ba03949f94970b9c1dd7cdd1fd4ad11b4e3e
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-15 20:28:37 +0000

    edit abs

--- python/abs.py
+++ python/abs.py
@@ -5,3 +5,20 @@ number = -20
 
 absolute_number = abs(number)
 print(absolute_number)
+
+# Приклад №1: Отримання абсолютного значення числа
+
+# Випадкове ціле число
+integer = -20
+print('Absolute value of -20 is:', abs(integer))
+
+# Випадкове число з плаваючою крапкою
+floating = -30.33
+print('Absolute value of -30.33 is:', abs(floating))
+
+
+# Приклад №2: Отримання величини комплексного числа
+
+# Випадкове комплексне число
+complex = (3 - 4j)
+print('Magnitude of 3 - 4j is:', abs(complex))
