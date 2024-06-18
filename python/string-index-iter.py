0bbe5a8d027345570ff5628e42f7e7656829b6ae
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-18 12:07:30 +0000

    edit string

---
+++ python/string-index-iter.py
@@ -0,0 +1,22 @@
+# Наприклад, якщо ви хочете отримати доступ до будь-якого символу рядка, ви можете зробити це за допомогою індексування
+
+# Indexing strings.
+
+the_string = 'silly walks'
+
+for ix in range(len(the_string)):
+    print(the_string[ix], end=' ')
+
+print()
+
+
+# Ітерація рядків теж працює.
+
+# Iterating through a string.
+
+the_string = 'silly walks'
+
+for character in the_string:
+    print(character, end=' ')
+
+print()
