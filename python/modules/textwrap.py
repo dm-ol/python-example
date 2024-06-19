eb8151fd63828cdd06f7df6dea83a623650aa6d0
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-19 20:44:59 +0000

    edit textwrap

--- python/modules/textwrap.py
+++ python/modules/textwrap.py
@@ -7,3 +7,9 @@ text = """The textwrap module can be used to format text for output in
 situations where pretty-printing is desired."""
 
 textwrap.fill(text, width=20)
+
+
+# Скорочення тексту. Першим аргументом передається рядок, другим вказуємо кількість символів,
+# що має містити результат. Також третім аргументом можна передати заготівлю,
+# яка вставлятиметься наприкінці обрізаного рядка.
+textwrap.shorten(text, 40, placeholder='... ')
