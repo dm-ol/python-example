6e8f8f1226776da198fb5c255a605d8b2c6a1486
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-27 20:34:12 +0000

    edit divmod

--- python/divmod.py
+++ python/divmod.py
@@ -7,3 +7,30 @@
 result = divmod(8, 3)
 
 print('Quotient and Remainder = ', result)
+
+
+# Функція divmod() в Python із цілими аргументами
+
+# Функція divmod() із цілочисельними аргументами
+print('divmod(8, 3) = ', divmod(8, 3))
+
+# Функція divmod() із цілочисельними аргументами
+print('divmod(3, 8) = ', divmod(3, 8))
+
+# Функція divmod() із цілочисельними аргументами
+print('divmod(5, 5) = ', divmod(5, 5))
+
+
+# Функція divmod() в Python з дробовими аргументами
+
+# Функція divmod() з дробовими аргументами
+print('divmod(8.0, 3) = ', divmod(8.0, 3))
+
+# Функція divmod() з дробовими аргументами
+print('divmod(3, 8.0) = ', divmod(3, 8.0))
+
+# Функція divmod() з дробовими аргументами
+print('divmod(7.5, 2.5) = ', divmod(7.5, 2.5))
+
+# Функція divmod() з дробовими аргументами
+print('divmod(2.6, 0.5) = ', divmod(2.6, 0.5))
