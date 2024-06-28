df9b2d957ab0313292dae59d598eb97ccdf416f0
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-28 20:36:33 +0000

    edit property

--- python/property.py
+++ python/property.py
@@ -39,3 +39,31 @@ print(x.value)
 x.value = 'Acd'
 
 del x.value
+
+
+# Створення атрибута з функціями геттера, сеттера та делітера
+
+class Person:
+    def __init__(self, name):
+        self._name = name
+
+    def get_name(self):
+        print('Getting name')
+        return self._name
+
+    def set_name(self, value):
+        print('Setting name to ' + value)
+        self._name = value
+
+    def del_name(self):
+        print('Deleting name')
+        del self._name
+
+    # Встановлюємо властивість для використання методів get_name, set_name та del_name
+    name = property(get_name, set_name, del_name, 'Name property')
+
+
+p = Person('Adam')
+print("The name is:", p.name)
+p.name = 'John'
+del p.name
