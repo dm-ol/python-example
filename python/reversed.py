fbf9bd65b45d06913e53fd8f830b8ff45b753f32
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-29 15:27:03 +0000

    edit reversed

--- python/reversed.py
+++ python/reversed.py
@@ -4,3 +4,35 @@ seq_string = 'Python'
 
 # Виводимо рядок "задом наперед"
 print(list(reversed(seq_string)))
+
+# Функція reversed() у Python із вбудованими об’єктами послідовності
+
+seq_tuple = ('P', 'y', 't', 'h', 'o', 'n')
+
+# Інверсія кортежу
+print(list(reversed(seq_tuple)))
+
+seq_range = range(5, 9)
+
+# Інверсія діапазону
+print(list(reversed(seq_range)))
+
+seq_list = [1, 2, 4, 3, 5]
+
+# Інверсія списку
+print(list(reversed(seq_list)))
+
+
+# Функція reversed() в Python з користувацькими об’єктами
+
+class Vowels:
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    def __reversed__(self):
+        return reversed(self.vowels)
+
+
+v = Vowels()
+
+# Інверсія користувацького об'єкта v
+print(list(reversed(v)))
