65f43ea40580c9a47eb4ae1e2089525e9b0b2976
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-30 13:20:48 +0000

    edit OOP

--- python/OOP-class+object.py
+++ python/OOP-class+object.py
@@ -83,3 +83,30 @@ print(f"Employee ID: {employee1.employeeID}")
 # Отримуємо доступ до атрибутів об'єкта employee2 та присвоюємо нове значення
 employee2.employeeID = 1002
 print(f"Employee ID: {employee2.employeeID}")
+
+
+# Функція, визначена всередині класу, називається методом.
+# Тут ми створили клас Room з атрибутами: length та width; та методом calculate_area().
+# Потім ми створили об’єкт study_room класу Room та використали цей об’єкт для присвоєння
+# значень атрибутам length та width. Зверніть увагу, що ми також використали цей об’єкт
+# для виклику методу всередині класу за допомогою оператора
+
+# Створюємо клас
+class Room:
+    length = 0.0
+    width = 0.0
+
+    # Метод обчислення площі
+    def calculate_area(self):
+        print("Area of Room =", self.length * self.width)
+
+
+# Створюємо об'єкт класу Room
+study_room = Room()
+
+# Надаємо нові значення всім атрибутам об'єкта
+study_room.length = 42.5
+study_room.width = 30.8
+
+# Отримуємо доступ до методу всередині класу
+study_room.calculate_area()
