97e66611413dd0782dd5a817f737686e7df36727
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-30 13:19:38 +0000

    edit OOP

--- python/OOP-class+object.py
+++ python/OOP-class+object.py
@@ -43,3 +43,43 @@ bike1.name = "Mountain Bike"
 # Отримуємо доступ до атрибута gear об'єкта bike1
 bike1.gear
 # Тут ми використали bike1.name та bike1.gear для доступу та зміни значень атрибутів name та gear.
+
+# Створюємо клас
+
+
+class Bike:
+    name = ""
+    gear = 0
+
+
+# Створюємо об'єкт класу
+bike1 = Bike()
+
+# Отримуємо доступ до атрибутів об'єкта та присвоюємо нові значення
+bike1.gear = 11
+bike1.name = "Mountain Bike"
+
+# Виводимо дані об'єкта на екран
+print(f"Name: {bike1.name}, Gears: {bike1.gear} ")
+
+
+# Створення кількох об’єктів одного класу
+# Ми можемо створити багато об’єктів одного класу. Наприклад:
+
+# Створюємо клас
+class Employee:
+    # Визначаємо атрибут
+    employee_id = 0
+
+
+# Створюємо два об'єкти класу Employee
+employee1 = Employee()
+employee2 = Employee()
+
+# Отримуємо доступ до атрибутів об'єкта employee1 та присвоюємо нове значення
+employee1.employeeID = 1001
+print(f"Employee ID: {employee1.employeeID}")
+
+# Отримуємо доступ до атрибутів об'єкта employee2 та присвоюємо нове значення
+employee2.employeeID = 1002
+print(f"Employee ID: {employee2.employeeID}")
