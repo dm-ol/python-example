5ccf53644c94527dac5ef893dadaf8059b296c02
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-30 13:32:56 +0000

    edit all

--- python/all.py
+++ python/all.py
@@ -39,3 +39,21 @@ print(all(s))
 
 s = ''
 print(all(s))
+
+# Як функція all() працює зі словниками в Python?
+
+s = {0: 'False', 1: 'False'}
+print(all(s))
+
+s = {1: 'True', 2: 'True'}
+print(all(s))
+
+s = {1: 'True', False: 0}
+print(all(s))
+
+s = {}
+print(all(s))
+
+# '0' рівний True
+s = {'0': 'True'}
+print(all(s))
