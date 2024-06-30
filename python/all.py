e820df2230d98ade49516fca6f8a3ad762d1d8a6
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-06-30 13:31:22 +0000

    add all

---
+++ python/all.py
@@ -0,0 +1,7 @@
+# Функція all() повертає True, якщо всі елементи заданого ітерованого об’єкта дорівнюють True. Якщо ні, повертається False.
+
+boolean_list = ['True', 'True', 'True']
+
+# Перевіряємо, чи всі елементи дорівнюють True
+result = all(boolean_list)
+print(result)
