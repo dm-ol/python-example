1d86c89e7918a2ba5df5121a2a3c029f65833e87
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-07-02 21:33:32 +0000

    edit string

--- python/strin-sort.py
+++ python/strin-sort.py
@@ -2,6 +2,7 @@
 # Перший реалізований як функція з іменем sorted().
 
 # Demonstrating the sorted() function:
+from math import pi
 first_greek = ['omega', 'alpha', 'pi', 'gamma']
 first_greek_2 = sorted(first_greek)
 
@@ -19,3 +20,31 @@ print(second_greek)
 
 second_greek.sort()
 print(second_greek)
+
+
+# f-рядки — зручний спосіб включення значення виразу всередині рядків (з версії 3.6).
+
+# 🔴str.format() — повертає копію рядка, в якому на місці {} буде поставлено зазначені аргументи.
+
+# 🔴оператор % — форматування по-старому: в стилі C.
+
+
+s = f'pi: {pi: .3f}'    # pi: 3.142
+print(s)
+
+num = 714
+s = f'ThePyU: {num}'    # 'ThePyU: 714'
+print(s)
+
+d = {'one': 1}
+s = f"one: {d['one']}"    # 'one: 1'
+print(s)
+
+s = '''The {} Universe:
+{value}'.format('Python', value=714)
+# 'The Python Universe: 714'''
+print(s)
+
+num = 4
+s = 'string: %s' % num    # 'string: 4'
+print(s)
