6b0a6d7fb7dcc627868826dbb05a7d74e3c9c6bc
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-07-15 20:29:19 +0000

    edit stack

--- python/stack-OOP.py
+++ python/stack-OOP.py
@@ -1,5 +1,13 @@
 # Стек - об'єктний підхід
 
+class Stack:  # Defining the Stack class.
+    def __init__(self):  # Defining the constructor function.
+        print("Hi!")
+
+
+stack_object = Stack()  # Instantiating the object.
+
+
 class Stack:
     def __init__(self):
         self.stack_list = []
@@ -7,3 +15,16 @@ class Stack:
 
 stack_object = Stack()
 print(len(stack_object.stack_list))
+
+# Якщо будь-який компонент класу має назву, що починається з двох підкреслень (__),
+# він стає приватним – це означає, що до нього можна отримати доступ лише з класу.
+# Incapsulation
+
+
+class Stack:
+    def __init__(self):
+        self.__stack_list = []
+
+
+stack_object = Stack()
+print(len(stack_object.__stack_list))
