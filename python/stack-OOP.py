1c34aee56d984454d555f3d69a7e65bcfcef94d6
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-07-15 20:44:17 +0000

    edit stack

--- python/stack-OOP.py
+++ python/stack-OOP.py
@@ -77,3 +77,59 @@ stack_object_1.push(3)
 stack_object_2.push(stack_object_1.pop())
 
 print(stack_object_2.pop())
+
+# Example 2
+
+
+class Stack:
+    def __init__(self):
+        self.__stack_list = []
+
+    def push(self, val):
+        self.__stack_list.append(val)
+
+    def pop(self):
+        val = self.__stack_list[-1]
+        del self.__stack_list[-1]
+        return val
+
+
+little_stack = Stack()
+another_stack = Stack()
+funny_stack = Stack()
+
+little_stack.push(1)
+another_stack.push(little_stack.pop() + 1)
+funny_stack.push(another_stack.pop() - 2)
+
+print(funny_stack.pop())
+
+#  визначте новий підклас, який вказує на клас, який використовуватиметься як суперклас
+
+
+class AddingStack(Stack):
+    def __init__(self):
+        Stack.__init__(self)
+        self.__sum = 0
+
+    def get_sum(self):
+        return self.__sum
+
+    def push(self, val):
+        self.__sum += val
+        Stack.push(self, val)
+
+    def pop(self):
+        val = Stack.pop(self)
+        self.__sum -= val
+        return val
+
+
+stack_object = AddingStack()
+
+for i in range(5):
+    stack_object.push(i)
+print(stack_object.get_sum())
+
+for i in range(5):
+    print(stack_object.pop())
