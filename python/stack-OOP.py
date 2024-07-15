7988cc486bb8855bf682bd22f61bbb59f0f0a003
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-07-15 20:32:15 +0000

    edit stack

--- python/stack-OOP.py
+++ python/stack-OOP.py
@@ -27,4 +27,29 @@ class Stack:
 
 
 stack_object = Stack()
-print(len(stack_object.__stack_list))
+# print(len(stack_object.__stack_list))
+
+
+# push and pop
+class Stack:
+    def __init__(self):
+        self.__stack_list = []
+
+    def push(self, val):
+        self.__stack_list.append(val)
+
+    def pop(self):
+        val = self.__stack_list[-1]
+        del self.__stack_list[-1]
+        return val
+
+
+stack_object = Stack()
+
+stack_object.push(3)
+stack_object.push(2)
+stack_object.push(1)
+
+print(stack_object.pop())
+print(stack_object.pop())
+print(stack_object.pop())
