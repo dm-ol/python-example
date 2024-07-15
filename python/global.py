27dff64abe9b72a4183631f43d1129685b91f47a
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-07-15 03:10:16 +0000

    some edit

--- python/global.py
+++ python/global.py
@@ -17,3 +17,27 @@ func_1()
 
 print(a)
 print(b)
+
+# Example 2
+
+a = 1
+
+
+def foo_1():
+    a = 1 + 2
+    print(a, end=' ')
+
+
+def foo_2():
+    global a
+    a = 1 + 2
+    print(a, end=' ')
+
+
+foo_1()
+print(a)
+# 3 1
+
+foo_2()
+print(a)
+# 3 3
