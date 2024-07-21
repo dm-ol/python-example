6b2f91a5cebe3bdf09e420ddef1e917373a44606
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-07-21 19:41:28 +0000

    edit staticmethod

--- python/staticmethod.py
+++ python/staticmethod.py
@@ -1,4 +1,9 @@
 # Функція staticmethod() повертає статичний метод для цієї функції.
+# Коли слід використовувати статичні методи?
+# 1. Допоміжна функція для класу
+# Статичні методи мають обмежений варіант використання, оскільки, як і методи класу або будь-які інші методи всередині класу, вони не можуть отримати доступ до властивостей самого класу.
+
+# Однак, коли потрібна допоміжна функція, яка не звертається до жодних властивостей класу, але потрібна її приналежність до класу, слід використовувати статичні методи.
 
 class Calculator:
 
@@ -20,3 +25,41 @@ print('Sum:', sum)
 # Вони можуть бути спричинені як самим класом, так і його об’єктом:
 #   Class.staticmethodFunc()
 #   Class().staticmethodFunc()
+
+# Створення статичного методу за допомогою функції staticmethod()
+
+
+class Mathematics:
+
+    def addNumbers(x, y):
+        return x + y
+
+
+# Створюємо статичний метод addNumbers()
+Mathematics.addNumbers = staticmethod(Mathematics.addNumbers)
+
+print('The sum is:', Mathematics.addNumbers(5, 10))
+
+
+# Створення допоміжної функції в якості статичного методу
+
+class Dates:
+    def __init__(self, date):
+        self.date = date
+
+    def getDate(self):
+        return self.date
+
+    @staticmethod
+    def toDashDate(date):
+        return date.replace("/", "-")
+
+
+date = Dates("15-12-2016")
+dateFromDB = "15/12/2016"
+dateWithDash = Dates.toDashDate(dateFromDB)
+
+if (date.getDate() == dateWithDash):
+    print("Equal")
+else:
+    print("Unequal")
