64375db484229d62291d06c535fe226ec51be764
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-07-23 11:06:06 +0000

    edit delattr

--- python/delattr.py
+++ python/delattr.py
@@ -27,3 +27,25 @@ print('z = ', point1.z)
 
 
 # Видалення атрибута з використанням оператора del
+
+class Coordinate:
+    x = 10
+    y = -5
+    z = 0
+
+
+point1 = Coordinate()
+
+print('x = ', point1.x)
+print('y = ', point1.y)
+print('z = ', point1.z)
+
+# Видаляємо атрибут z
+del Coordinate.z
+
+print('--After deleting z attribute--')
+print('x = ', point1.x)
+print('y = ', point1.y)
+
+# Викличе помилку
+print('z = ', point1.z)
