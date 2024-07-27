340c675c60378125c72b6d3661dbce243cf8d9eb
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-07-27 08:41:57 +0000

    add bytearray

---
+++ python/bytearray.py
@@ -0,0 +1,7 @@
+# Функція bytearray() повертає об’єкт bytearray, який є масивом вказаних байтів.
+
+prime_numbers = [2, 3, 5, 7]
+
+# Конвертуємо список у bytearray
+byte_array = bytearray(prime_numbers)
+print(byte_array)
