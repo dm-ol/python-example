7bdf52f61363aaa5a847493878f129816542b3fb
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-07-28 14:33:57 +0000

    add len

---
+++ python/len.py
@@ -0,0 +1,7 @@
+# Функція len() повертає кількість елементів (довжину) об’єкта.
+
+languages = ['Python', 'Java', 'JavaScript']
+
+# Обчислюємо довжину списку languages
+length = len(languages)
+print(length)
