cd17e04c30b293aa8d092a41ddbff6ecb383838f
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-07-28 14:36:24 +0000

    edit len

--- python/len.py
+++ python/len.py
@@ -39,3 +39,43 @@ testList = [1, 2, 3]
 # Конвертуємо в байтовий об'єкт
 testByte = bytes(testList)
 print('Length of', testByte, 'is', len(testByte))
+
+
+# Як функція len() працює зі словниками та множинами?
+
+testSet = {1, 2, 3}
+print(testSet, 'length is', len(testSet))
+
+# Порожня множина
+testSet = set()
+print(testSet, 'length is', len(testSet))
+
+testDict = {1: 'one', 2: 'two'}
+print(testDict, 'length is', len(testDict))
+
+testDict = {}
+print(testDict, 'length is', len(testDict))
+
+testSet = {1, 2}
+# Заморожена множина
+frozenTestSet = frozenset(testSet)
+print(frozenTestSet, 'length is', len(frozenTestSet))
+
+
+# Як працює функція len() з об’єктами користувача?
+
+class Session:
+    def __init__(self, number=0):
+        self.number = number
+
+    def __len__(self):
+        return self.number
+
+
+# Довжина за замовчуванням дорівнює 0
+s1 = Session()
+print(len(s1))
+
+# Вказуємо довільну довжину
+s2 = Session(6)
+print(len(s2))
