4545a32374f87bb2c25d0ff73a6299e7ce6ed11c
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-08-11 11:15:48 +0000

    edit re

--- python/modules/re.py
+++ python/modules/re.py
@@ -15,3 +15,30 @@ print(is_allowed("abyzABYZ0099"))
 # True
 print(is_allowed("#*@#$%^"))
 # False
+
+
+# Для виконання текстових операцій без урахування регістру можна використовувати модуль re та вказати прапор re.IGNORECASE для різних операцій.
+
+text = 'UPPER PYTHON, lower python, Mixed Python'
+print(re.findall('python', text, flags=re.IGNORECASE))
+# ['PYTHON', 'python', 'Python']
+print(re.sub('python', 'snake', text, flags=re.IGNORECASE))
+# UPPER snake, lower snake, Mixed snake
+
+
+def matchcase(word):
+    def replace(m):
+        text = m.group()
+        if text.isupper():
+            return word.upper()
+        elif text.islower():
+            return word.lower()
+        elif text[0].isupper():
+            return word.capitalize()
+        else:
+            return word
+    return replace
+
+
+print(re.sub('python', matchcase('snake'), text, flags=re.IGNORECASE))
+# UPPER SNAKE, lower snake Mixed Snake
