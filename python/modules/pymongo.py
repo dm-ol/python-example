6f601e4734ec81fb21cba7ec17c23f86b88770c9
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-10-26 20:37:58 +0000

    add pymongo

---
+++ python/modules/pymongo.py
@@ -0,0 +1,14 @@
+# Щоб вставити документ у колекцію, використовуйте метод insert one().
+# Перший параметр методу insert one() — це словник, що містить імена та значення кожного поля в документі, який ви хочете вставити.
+
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydict = {"name": "John", "address": "Highway 37"}
+
+x = mycol.insert_one(mydict)
+
+# Метод повертає об'єкт InsertOneResult з властивістю insert id, що містить ідентифікатор вставленого документа.
