07c36f2a0cd234d47901d2204e49c73e8d5f0bd5
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-11-04 12:59:11 +0000

    edit pymongo

--- python/modules/pymongo.py
+++ python/modules/pymongo.py
@@ -12,3 +12,20 @@ mydict = {"name": "John", "address": "Highway 37"}
 x = mycol.insert_one(mydict)
 
 # Метод повертає об'єкт InsertOneResult з властивістю insert id, що містить ідентифікатор вставленого документа.
+
+
+# Щоб зробити розширені запити, можна використовувати модифікатори як значення в об'єкті запиту.
+
+
+myclient = pymongo.MongoClient("mongodb: //localhost: 27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+myquery = {"address": {"$gt": "S"}}
+
+mydoc = mycol.find(myquery)
+
+for x in mydoc:
+    print(x)
+
+# Наприклад, щоб знайти документи, в яких поле "адреса" починається з літери "S" або вище (за абеткою), використовуйте модифікатор "більше, ніж": {"$gt": "S"}.
